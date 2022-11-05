1fe09057a03e27e766fc8527e39a986b1956ab7c
Osama Bin Azam <osamabinazam@gmail.com>
2022-11-05 12:36:21 +0500

    Learned about input function, putting one function into another function and being able to get input from console

---
+++ Day 1/day_1_input.py
@@ -0,0 +1,7 @@
+
+# input function return what is gets from user
+# input () get user input in console
+# print () prints that input to console and also other string that are written in print function's parenthesis
+
+print("Hello " + input("What is your name ? ") + "!")
+print("Your are " + input("What is your age ? ") + " years old.")
