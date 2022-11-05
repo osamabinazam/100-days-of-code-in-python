ba1f1f04ec8748495dcff9ba2a0c356a96c78b97
Osama Bin Azam <osamabinazam@gmail.com>
2022-11-05 12:16:15 +0500

    Day 1 -  Pyhton Code  (String Manipulation)

--- Day 1/day_1_string_manipulation.py
+++ Day 1/day_1_string_manipulation.py
@@ -1,2 +1,8 @@
 # printing Hello World! three times on different line using one single print statement
 print("Hello World!\nHello World!\nHello World!")
+
+# concatenating two strings and printing them on console as a single string
+# Below three line do same
+print("Hello" + " Osama")
+print("Hello " + "Osama")
+print("Hello" + " " +"Osama")
