597fa9c6b773d9e50fcc5cd45fa56597f8fd9dbd
Osama Bin Azam <osamabinazam@gmail.com>
2023-02-28 16:44:59 +0500

    File Processing (Updated)

--- Practice/Extras/Filing.py
+++ Practice/Extras/Filing.py
@@ -2,7 +2,7 @@
 # Open () function create an object of file and returned it
 # Returned Object has a function called read that help to read content of the file and read() return content as a String Object
 
-myfile = open("fruits.txt", mode="r")
+myfile = open("./files/fruits.txt", mode="r")
 content = myfile.read()
 print("Type of myfile object is : " , type(myfile))             # return the name of the classs
 myfile.close()                                                  # close the file object
@@ -13,7 +13,7 @@ print("Content of the file is : \n" ,content)
 
 # Optimum and Orgranized way in which we don't have to close file object manually
 # Process file here and then close
-with open("fruits.txt" , mode="r") as myfile:
+with open("./files/fruits.txt" , mode="r") as myfile:
     content = myfile.read()
 
 
@@ -22,20 +22,20 @@ print("\nContent of the file is : \n", content)
 # Writing content to file
 # "w" -> If file is exist then the content of the file will be overridden
 # "x" -> If file is exist then an error will be thrown
-with open("vagetables.txt" , mode="w") as myfile:
+with open("./files/vagetables.txt" , mode="w") as myfile:
     myfile.write("Tomato\nCarrot\nBringle")
 
 
 # Appending content to file
 # "a" -> use to append con tent to the file and updates it
-with open("vagetables.txt" , "a") as myfile:
+with open("./files/vagetables.txt" , "a") as myfile:
     myfile.write("\nGarlic")
 
 del myfile
 # we can not read from and write to file using same object.
 # To do so, we use "a+" mode which let us write/read operation on a same object
 # we also use seek() method that let us change the position of the cursur
-with open("fruits.txt" ,"a+") as myfile:
+with open("./files/fruits.txt" ,"a+") as myfile:
     myfile.write("\nWatermelon")
     content = myfile.read()
 print("The content of the file is : " , content)
